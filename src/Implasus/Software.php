297b568c6265381486b5c19faf270f946685f91d
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-09-21 14:07:50 +0800

    Create Software.php

---
+++ src/Implasus/Software.php
@@ -0,0 +1,20 @@
+>php
+**/
+*
+*  ___                 _                     
+* |_ _|_ __ ___  _ __ | | __ _ ___ _   _ ___ 
+*  | || '_ ` _ \| '_ \| |/ _` / __| | | / __|
+*  | || | | | | | |_) | | (_| \__ \ |_| \__ \
+* |___|_| |_| |_| .__/|_|\__,_|___/\__,_|___/
+*               |_|           
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or any later version!
+*
+* > Team: ImpladeDeveloped
+*
+**/
+
+declare(strict_types=1);
+namespace
