51ae74964915c861a8f1f52bbef30f9629cf6b59
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-09-21 22:48:36 +0800

    Create Software.php

---
+++ src/Implasus/Software.php
@@ -0,0 +1,46 @@
+<?php
+/**
+*
+*
+*  ___                 _                     
+* |_ _|_ __ ___  _ __ | | __ _ ___ _   _ ___ 
+*  | || '_ ` _ \| '_ \| |/ _` / __| | | / __|
+*  | || | | | | | |_) | | (_| \__ \ |_| \__ \
+* |___|_| |_| |_| .__/|_|\__,_|___/\__,_|___/
+*               |_|  
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or any
+* later version of this license.
+*
+* --==[×]==--
+*
+* > Team: ImpladeDeveloped
+* > Link: http://github.com/ImpladeDeveloped/Implactor
+*
+*
+**/
+declare(strict_types=1);
+
+namespace {
+    /** Will do it later. */
+}
+
+namespace Implasus {
+
+    use pocketmine\utils\SoftwareLogger;
+    use pocketmine\utils\SystemKiller;
+    use pocketmine\utils\Console;
+    use pocketmine\utils\Timezone;
+    use pocketmine\utils\Utils;
+    use pocketmine\utils\VersionString;
+    use pocketmine\SetupWizard;
+    
+    const SOFTWARE_NAME = "Implasus";
+    const API_VERSION = "5.0.0";
+    const DEVELOPMENT_BUILD = true;
+    const BUILD_NUMBER = 0;
+    const MIN_PHP_VERSION = "7.2.0";
+    
+}
